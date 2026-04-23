b3a125bf2c30827750bf24603a5bac50f5b62e43
Farhd <farhad.norman@gmail.com>
2026-04-23 18:52:53 +0200

    Fas1-grunder: print, variabler och matematik

---
+++ grunderna.py
@@ -0,0 +1,41 @@
+
+# Print()
+print("Hej! Jag lär mig Python!")
+print("Mitt namn är Farre")
+print("Jag studerar IT-säkerhet")
+
+# Variabler
+namn = "Farre"
+alder = 38
+stad = "Uppsala"
+
+print("Namn:", namn)
+print("Ålder:", alder)
+print("Stad:", stad)
+
+# Olika typer av variabler!
+
+text = "Jag är en text"     # String
+heltal = 38                 # Integer
+decimaltal = 4.5            # Float
+sant_eller_falsk = True     # Boolean
+
+#Type() visar vilken typ variabeln är
+
+print(type(text))
+print(type(heltal))
+print(type(decimaltal))
+print(type(sant_eller_falsk))
+
+# Matematik 
+
+x = 10
+y = 3
+
+print("Addition:",         x + y)      # 13
+print("Subtrakton:",       x - y)      # 7
+print("Multiplikation:",   x * y)      # 30
+print("Division:",         x / y)      # 3.333...
+print("Heltalsdivision:",  x // y)       # 3
+print("Rest:",             x % y)      # 1
+print("Upphöjt till:",     x ** y)     # 1000
